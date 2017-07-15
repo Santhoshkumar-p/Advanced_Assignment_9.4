90a81292d46d538f188918351933955447725467
Santhoshkumar-p <sanssoft12@gmail.com>
2017-07-15 18:28:28 +0530

    Add files via upload

---
+++ app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { HighlightDirective } from './highlight.directive';
+import { UnlessDirective } from './unless.directive';
+
+@Component({
+  selector: 'dir-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  private switch=true;
+  private items=[1,2,3,4,5];
+  private value=10;
+  onSwitch(){
+this.switch=!this.switch
+  }
+} 
+
---
+++ app/highlight.directive.ts
@@ -0,0 +1,31 @@
+import { Directive,ElementRef,Renderer ,HostListener,HostBinding,Input,OnInit} from '@angular/core';
+
+@Directive({
+  selector: '[highlight]'
+})
+export class HighlightDirective {
+  @HostListener('mouseenter') mouseover(){
+  this.backgroundColor=this.highlightColor;
+  };
+   @HostListener('mouseleave') mouseleave(){
+  this.backgroundColor=this.defaultColor;
+  };
+
+  @HostBinding('style.background') get setColor(){
+    return this.backgroundColor;
+  };
+  @Input() defaultColor="white";
+  @Input('highlight') highlightColor="green";
+  private backgroundColor:string;
+  constructor(private elementRef: ElementRef,private renderer:Renderer) { 
+  
+    //this.elementRef.nativeElement.style.backgroundColor='green';
+    //this.renderer.setElementStyle(this.elementRef.nativeElement,'background-color', 'green');
+  }
+ngOnInit(){
+  this.backgroundColor=this.defaultColor;
+}
+}
+
+
+  
